6a2d487a96809afd028f5723c7277e25c409d045
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-01-24 17:37:43 +0100

    Animation Unit-5

--- Unit-5 Animaciones/Ejercicios y prácticas/Ejercicios Animaciones/Ejercicio 3/animation.js
+++ Unit-5 Animaciones/Ejercicios y prácticas/Ejercicios Animaciones/Ejercicio 3/animation.js
@@ -20,19 +20,23 @@ function myFunction() {
   }
 }
 
-(function() {
-  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+(function () {
+  var requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
   window.requestAnimationFrame = requestAnimationFrame;
 })();
 
 var start = null;
-var element = document.getElementById('train');
+var element = document.getElementById("train");
 
 function step(timestamp) {
   if (!start) start = timestamp;
   var progress = timestamp - start;
-  element.style.transform = 'translateX(' + Math.min(progress / 10, 200) + 'px)';
+  element.style.transform =
+    "translateX(" + Math.min(progress / 10, 200) + "px)";
   if (progress < 2000) {
     window.requestAnimationFrame(step);
   }
