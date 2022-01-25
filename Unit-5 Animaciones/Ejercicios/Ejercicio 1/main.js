71cc47882dc8ef1f38570c2e838c6de7112a6e51
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-01-25 20:45:57 +0100

    Web components

--- Unit-5 Animaciones/Ejercicios/Ejercicio 1/main.js
+++ Unit-5 Animaciones/Ejercicios/Ejercicio 1/main.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var divs = document.querySelectorAll("div.animate");
+  //Div with animate class
+  var containers = document.querySelectorAll("div.animate");
   var arrayID = [];
 
-  // Los divs vuelven a permanecer ocultos y se reinicia la animaci�n
-  function reiniciar() {
-    // Se vac�a el array con setTimeout
+  function resetAnimation() {
     arrayID = [];
 
-    divs.forEach((curDIV) => {
-      curDIV.style.display = "none";
+    containers.forEach((currentContainer) => {
+      currentContainer.style.display = "none";
     });
-    aparecer();
+    startAnimation();
   }
-  function aparecer() {
-    divs.forEach((elem, index) => {
+
+  function startAnimation() {
+    containers.forEach((elem, index) => {
       // Se ejecuta un setTimeout por cada ID cada vez con un milisegundo de retardo m�s: 0, 1000, 2000, 3000, etc
       let newID = setTimeout(() => {
         elem.style.display = "block"; // Por cada setTimeout mostramos el bot�n
@@ -22,17 +22,16 @@ document.addEventListener("DOMContentLoaded", () => {
       arrayID.push(newID);
     });
   }
+  startAnimation();
 
-  aparecer();
-
-  divs.forEach((elem) => {
+  //Add click function to all the containers
+  containers.forEach((elem) => {
     elem.addEventListener("click", () => {
       // Al hacer click en cada DIV podemos finalizar todos los setTimeout pendientes
       arrayID.forEach((curID) => {
         clearTimeout(curID);
       });
-
-      reiniciar();
+      resetAnimation();
     });
   });
 });
