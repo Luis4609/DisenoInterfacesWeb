1887ff5807270289c694da9f22cb6be6e07af7e7
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-02-04 19:04:50 +0100

    Ejercicio 2 Web components

--- Unit-5 Animaciones/Ejercicio WebComponent/Ejercicio 2/main.js
+++ Unit-5 Animaciones/Ejercicio WebComponent/Ejercicio 2/main.js
@@ -3,20 +3,36 @@ const container = document.getElementById("container");
 const mySwitch = document.getElementById("switch");
 const backgroundColor = document.getElementById("color-fondo");
 
+mySwitch.settings = "Off";
+
+console.log(mySwitch.settings);
 mySwitch.addEventListener("click", function () {
-  container.style.flexDirection = "row-reverse";
-  backgroundColor.style.backgroundColor = "green";
-  mySwitch.innerText = "On";
+    if(this.settings == "Off"){
+        container.style.flexDirection = "row-reverse";
+        backgroundColor.style.backgroundColor = "green";
+        mySwitch.innerText = "On";
+        mySwitch.settings = "On";
+        console.log("If");
+        console.log(mySwitch.settings);
+    }else{
+        container.style.flexDirection = "row";
+        backgroundColor.style.backgroundColor = "red";
+        mySwitch.innerText = "Off";
+        mySwitch.settings = "Off";
+        console.log("Else");
+    }
+ 
 });
 
-function changeSwitch() {
-  if ((backgroundColor.style.backgroundColor = "green")) {
-    backgroundColor.style.backgroundColor = "red";
-    mySwitch.innerText = "Off";
-  } else {
-    backgroundColor.style.backgroundColor = "green";
-    mySwitch.innerText = "On";
-  }
-}
+// function changeSwitch() {
+//   if ((backgroundColor.style.backgroundColor = "green")) {
+//     backgroundColor.style.backgroundColor = "red";
+//     mySwitch.innerText = "Off";
+//   } else {
+//     backgroundColor.style.backgroundColor = "green";
+//     mySwitch.innerText = "On";
+//   }
+// }
 
-mySwitch.addEventListener("onclick", changeSwitch());
+// mySwitch.addEventListener("onclick", changeSwitch());
+
